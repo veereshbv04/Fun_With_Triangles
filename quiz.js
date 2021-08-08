0b1141742a091b698035f15fe3bf75d16cf74db3
Veeresh B V <Veereshbv04@gmail.com>
2021-08-08 20:09:10 +0530

    feat:quiz working properly

--- quiz.js
+++ quiz.js
@@ -5,7 +5,25 @@ const submitButton = document.querySelector(".submit-button");
 const correctAnswers = ["Yes", "No"];
 
 function checkResponse(){
+    const formResults = new FormData(quizForm);
+    // console.log( typeof formResults.entries);   function it is
+    // console.log( typeof formResults.entries());   object it is
+
+    let score = 0;
+    let index = 0;
+    for(let answer of formResults.values()){
+        console.log(answer,correctAnswers[index]);
+        if(answer ===  correctAnswers[index]){
+            score = score +1
+        }
+        index = index + 1;
+    }
+    console.log(score);
+    // outputDiv.innertText = ` fff${score}`;
+    outputDiv.innerText = `Your Score is ${score}`;
+
     
+     
 }
 
 submitButton.addEventListener("click",checkResponse);
