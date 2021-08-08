ccec90dfecb94db33a2bca4330515df0cf3b89b7
Veeresh B V <Veereshbv04@gmail.com>
2021-08-08 18:55:20 +0530

    fwat: till qiz form

---
+++ quiz.js
@@ -0,0 +1,11 @@
+const quizForm = document.querySelector(".quiz-form");
+const outputDiv = document.querySelector(".output-div");
+const submitButton = document.querySelector(".submit-button");
+
+const correctAnswers = ["Yes", "No"];
+
+function checkResponse(){
+    
+}
+
+submitButton.addEventListener("click",checkResponse);
